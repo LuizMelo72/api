c6fedcd2836e39ac5c76fff385619ecbeef013b3
Luiz Melo <luizhmelo@hotmail.com>
2023-01-24 13:59:25 -0300

    Added another jest test

--- src/services/UserCreateService.spec.js
+++ src/services/UserCreateService.spec.js
@@ -1,7 +1,16 @@
 const UserCreateService = require('./UserCreateService');
 const UserRepositoryInMemory = require('../repositories/UserRepositoryInMemory');
+const AppError = require("../utils/AppError");
 
 describe("UserCreateService", () => {
+    let userRepositoryInMemory = null;
+    let userCreateService = null;
+
+    beforeEach(() => {
+        userRepositoryInMemory = new UserRepositoryInMemory();
+        userCreateService = new UserCreateService(userRepositoryInMemory);
+    });
+
     it("User should be created", async () => {
         const user = {
             name: 'User test',
@@ -9,10 +18,28 @@ describe("UserCreateService", () => {
             password: "123"
         };
     
-        const userRepositoryInMemory = new UserRepositoryInMemory();
-        const userCreateService = new UserCreateService(userRepositoryInMemory);
+        
         const userCreated = await userCreateService.execute(user);
     
         expect(userCreated).toHaveProperty("id");
+    });
+
+    it("User should not be created with an existing email", async () => {
+        const user1 = {
+            name: "User Test 1",
+            email: "user@test.com",
+            password: "123"
+        };
+
+        const user2 = {
+            name: "User Test 2",
+            email: "user@test.com",
+            password: "123"
+        };
+
+        
+        await userCreateService.execute(user1);
+        await expect (userCreateService.execute(user2)).rejects.toEqual(new AppError("Este email já está em uso."));
+
     });
 })
